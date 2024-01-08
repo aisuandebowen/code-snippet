cdf22d702ffdaaa9315df1ec7f0e8aea9eef2b4c
Boen Chen <58322181+aisuandebowen@users.noreply.github.com>
2024-01-08 15:03:55 +0800

    Update utils.js

--- webpack/checkConsole/utils.js
+++ webpack/checkConsole/utils.js
@@ -17,8 +17,20 @@ function checkFile(file, typeArr) {
   return reg.test(file);
 }
 
+/**
+ * 检验路径在某规则下是否通过
+ * @param {String} rule 规则 例如：src/*,*.js
+ * @param {String} file 路径
+ * @returns
+ */
+function checkPath(rule, file) {
+  const formatStr = rule.replace("*", ".*").replace("/", "/");
+  return new RegExp(formatStr).test(file);
+}
+
 module.exports = {
   isArray,
   isString,
   checkFile,
+  checkPath,
 };
