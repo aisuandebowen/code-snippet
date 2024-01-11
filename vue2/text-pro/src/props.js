69ef3de85c66b23f1b08adbc7485e92300466d9d
Adam <58322181+aisuandebowen@users.noreply.github.com>
2024-01-11 17:21:56 +0800

    Create props.js

---
+++ vue2/text-pro/src/props.js
@@ -0,0 +1,27 @@
+export default {
+  // 是否开启toolTip
+  tip: {
+    default: false,
+    type: Boolean,
+  },
+  // toolTip文本
+  tipText: {
+    default: '',
+    type: String,
+  },
+  // 装饰性线条
+  textDecoration: {
+    default: 'none',
+    type: String,
+  },
+  // 是否省略 true => 1行
+  ellipsis: {
+    default: false,
+    type: Boolean,
+  },
+  // 省略行数
+  ellipsisNum: {
+    default: 1,
+    type: Number,
+  },
+};
